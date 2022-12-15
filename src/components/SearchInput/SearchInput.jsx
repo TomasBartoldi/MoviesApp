16b97eb4864aacc0f9766d1d101d603f8b65785b
Tomás <bartoldit@gmail.com>
2022-12-15 14:27:22 -0300

    Search styles finished, rendering on nav

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import { FaSearch } from 'react-icons/fa'
+import swAlert from '@sweetalert/with-react';
+
+
 
 const SearchInput = () => {
+
+    const submitHandler = e => {
+        e.preventDefault();
+        const keyword = e.currentTarget.keyword.value.trim();
+
+        if(keyword.length === 0){
+            swAlert(<h1>Please search something!!</h1>)
+        }
+    }
+
   return (
-    <form className='w-48 mr-10'>
+    <form className='w-48 mr-10' onSubmit={submitHandler} >
         <label>
-            <input className='rounded border-2 border-solid border-gray-900' type="text" />
+            <input className='rounded border-2 border-solid border-gray-900' type="text" name='keyword' placeholder='Search...' />
         </label>
         <button className='absolute right-12 top-4' type='submit'>
-           <FaSearch />
+           <FaSearch color={'gray'} />
         </button>
     </form>
   )
