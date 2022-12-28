0b731c3a0d76d695f0ff0053a7ce0d71b5851c44
Tomás <bartoldit@gmail.com>
2022-12-27 21:13:09 -0300

    target title, img, overview

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from './pages/Login'
 import Home from './pages/Home'
 import Detail from './pages/Detail'
@@ -10,10 +10,18 @@ import Results from './pages/Results'
 function App() {
 
    const addOrRemoveFromFavs = e => {
-      const btn = e.target;
-      const parent = btn.parentElement;
-      const imgURL = parent.querySelector('img').getAttribute('src')
-      console.log(imgURL);
+      /* const btn = e.target.parentElement;
+      const imgURL = btn.querySelector('img').getAttribute('src');
+      console.log(imgURL); */
+
+      //const btn = e.target;
+      //console.log(`soy el boton ${btn}`);
+      const image = document.getElementById('image').getAttribute('src')
+      const title = document.getElementById('title')
+      const title2 = e.currentTarget.title.value
+      const overview = document.getElementById('overview')
+      
+      console.log('soy el titulo', title2);
    }
 
   return (
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -42,15 +42,15 @@ const Listado = (props) => {
           md:max-w-2xl' key={index}>
             <div className='md:flex'>
               <div className='md:shrink-0'>
-                <img src={`https://image.tmdb.org/t/p/w500/${oneMovie.poster_path}`} className='h-full w-full object-cover md:h-full md:w-48' alt="img not found" />
+                <img id='image' src={`https://image.tmdb.org/t/p/w500/${oneMovie.poster_path}`} className='h-full w-full object-cover md:h-full md:w-48' alt="img not found" />
               </div>
               <div className='p-8'>
-                <div className='uppercase tracking-wide text-lg leading-tight font-semibold'>
+                <div id='title' className='uppercase tracking-wide text-lg leading-tight font-semibold'>
                    {oneMovie.title}
                 </div>
                 <hr />
                 <br />
-                <p>{oneMovie.overview.substring(0, 100)}...</p>
+                <p id='overview'>{oneMovie.overview.substring(0, 100)}...</p>
                 
                   <div className='w-auto flex flex-row justify-between items-center'>
                     <div>
